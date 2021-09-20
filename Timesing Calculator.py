7530e6d41247b8258ade3c205bd85e76bed60b0a
Connor Andrews <76876626+connorrandrews@users.noreply.github.com>
2021-09-20 10:08:30 +0100

    Update Timesing Calculator.py

--- Timesing Calculator.py
+++ Timesing Calculator.py
@@ -1,5 +1,9 @@
+#Created BY Connor Andrews
+#Grabbing the first number to times
 print('The first number to times?')
 timesby = input()
+#Getting the second number to times
 print('The second number to times by?')
 number = input()
+~Then Timesing them together
 print('The Answer is',int(timesby)*int(number))
